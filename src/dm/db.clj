67968f4622b7db4d83d3c19e2a546ee22fcd1ce9
Ivan Willig <iwillig@gmail.com>
2025-11-02 06:32:52 -0500

    Add more details to the database schema

--- src/dm/db.clj
+++ src/dm/db.clj
@@ -1,8 +1,37 @@
 (ns dm.db)
 
+(def species
+  #{:species/elf
+    :species/tiefling
+    :species/human
+    :species/orc
+    :species/halfing
+    :species/goliath
+    :species/gnome
+    :species/dwarf
+    :species/dragonborn
+    :species/aasimar})
+
+(def classes
+  #{:class/druid
+    :class/barbarian
+    :class/bard
+    :class/cleric
+    :class/fighter
+    :class/monk
+    :class/paladin
+    :class/ranger
+    :class/rouge
+    :class/sorcerer
+    :class/warlock
+    :class/wizard})
+
 (def schema
   {:attribute/name  {:db/valueType :db.type/keyword}
    :attribute/value {:db/valueType :db.type/long}
+
+   :item/name {:db/valueType :db.type/keyword}
+   :item/number {:db/valueType :db.type/log}
 
    :character/name        {:db/valueType :db.type/string}
    :character/armor-class {:db/valueType :db.type/long}
@@ -11,20 +40,17 @@
    :character/skill       {:db/valueType   :db.type/ref
                            :db/cardinality :db.cardinality/many}
 
-   :character/class {:db/valueType :db.type/ref}
-
-
-   :character/race {:db/valueType :db.type/ref}
+   :character/class   {:db/valueType :db.type/ref}
+   :character/species {:db/valueType :db.type/ref}
 
    :character/attributes {:db/valueType   :db.type/ref
                           :db/cardinality :db.cardinality/many}})
 
-
 (comment
 
   {:character/name       "Varis"
-   :character/race       :race/elf
-   :character/class      :class/druid
+   :character/species     :species/elf
+   :character/class       :class/druid
    :character/armor-class 4
    :character/attributes #{{:attribute/name  :attribute.name/strength
                             :attribute/value 10}
@@ -37,7 +63,4 @@
                            {:attribute/name  :attribute.name/constitution
                             :attribute/value 3}
                            {:attribute/name  :attribute.name/dexterity
-                            :attribute/value 3}}}
-
-
-  )
+                            :attribute/value 3}}})
